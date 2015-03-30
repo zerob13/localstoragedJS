978f711d8200857dfdcce97f3963c5a081e93dda
zerob13 <zerob13@gmail.com>
2015-03-30 11:39:45 +0800

    add script split

--- app/scripts/localstorage.js
+++ app/scripts/localstorage.js
@@ -4,6 +4,7 @@ var _lss = (
     var lss = {};
     var MAX_LENGTH = 1024 * 15;
     var MAX_ITEMS = Math.ceil((1024 * 1024 * 5) / MAX_LENGTH) - 1;
+    var KEY_PREFIX = 'ls2js_';
     console.log('max_l:' + MAX_LENGTH + ' max_items:' + MAX_ITEMS);
     lss.addScript = function(url) {
       $.ajax({
@@ -15,9 +16,26 @@ var _lss = (
         error: function(xhr, type) {}
       });
     }
-    function splitScript(data){
-      
+
+    function splitScript(data) {
+      var l = data.length;
+      var res = [];
+      if (l < MAX_LENGTH) {
+        res.push(data);
+      } else {
+        var num = Math.ceil(l / MAX_LENGTH);
+        for (i = 0; i < num; i++) {
+          if (i == num - 1) {
+            res.push(data.substring(i * MAX_LENGTH, l));
+          } else {
+            res.push(data.substring(i * MAX_LENGTH, (i + 1) * MAX_LENGTH));
+          }
+        }
+
+      }
+      return res;
     }
+
     return lss;
   }
 )();
